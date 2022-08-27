ea7cdc19f42bea93e73b022c66e7105edfec7d42
vidyaranjitha-hosadurga-raviprakash <100949435+vidyaranjitha-hosadurga-raviprakash@users.noreply.github.com>
2022-08-27 19:18:51 +0200

    fix: Updated the backend server url

--- src/lib/axiosClient.jsx
+++ src/lib/axiosClient.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosClient = axios.create({
-  baseURL: "http://localhost:5000/",
+  baseURL: "https://vhr-tracker.herokuapp.com/",
 });
 
 export default axiosClient;
